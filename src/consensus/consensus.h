2d532fbe9abcbeb6c27a99856b34322c0f7b4268
Cillian O’ Sullivan <81384331+cillian-osullivan@users.noreply.github.com>
2024-03-02 14:52:04 +0000

    Update consensus.h

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -10,18 +10,25 @@
 #include <stdint.h>
 
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
-static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 4000000;
+extern unsigned int dgpMaxBlockSerSize;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
-static const unsigned int MAX_BLOCK_WEIGHT = 4000000;
+extern unsigned int dgpMaxBlockWeight;
+
+extern unsigned int dgpMaxBlockSize; // globe
 
 
-/** The maximum allowed number of signature check operations in a block (network rule) */
-static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
+
+extern int64_t dgpMaxBlockSigOps;
+
+extern unsigned int dgpMaxProtoMsgLength;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
 
 static const int WITNESS_SCALE_FACTOR_PART = 2;
 static const int WITNESS_SCALE_FACTOR_GLB = 4;
+extern unsigned int dgpMaxTxSigOps;
+
+static const int MAX_TRANSACTION_BASE_SIZE = 1000000;
 extern int WITNESS_SCALE_FACTOR;
 
 static const size_t MIN_TRANSACTION_WEIGHT = WITNESS_SCALE_FACTOR * 60; // 60 is the lower bound for the size of a valid serialized CTransaction
@@ -32,5 +39,7 @@ static const size_t MIN_SERIALIZABLE_TRANSACTION_WEIGHT = WITNESS_SCALE_FACTOR *
 static constexpr unsigned int LOCKTIME_VERIFY_SEQUENCE = (1 << 0);
 
 static const size_t MAX_DATA_OUTPUT_SIZE = 512;
+
+void updateBlockSizeParams(unsigned int newBlockSize);
 
 #endif // GLOBE_CONSENSUS_CONSENSUS_H
